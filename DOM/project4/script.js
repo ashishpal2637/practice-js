a7f76f8c58c01357335506b570bb48c3f2ba0063
ashishpal2637 <ashishpal2637@gmail.com>
2025-08-07 13:28:07 +0530

    project4 half done

--- DOM/project4/script.js
+++ DOM/project4/script.js
@@ -1,8 +1,33 @@
-var crsr = document.querySelector('.crsr');
-var maindiv = document.querySelector('.main');
+var crsr = document.querySelector(".crsr");
+var maindiv = document.querySelector(".main");
+var logo = document.querySelector(".logo");
+var hero = document.querySelector("h1");
 
-maindiv.addEventListener('mousemove', function(dets){
-    crsr.style.left = dets.x + "px"
-    crsr.style.top = dets.y + "px"
-    
-})
+function customcrsr() {
+  maindiv.addEventListener("mousemove", function (dets) {
+    crsr.style.left = dets.x + "px";
+    crsr.style.top = dets.y + "px";
+  });
+}
+
+function heromouse() {
+  hero.addEventListener("mouseenter", function () {
+    crsr.style.scale = 5;
+  });
+  hero.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+  });
+}
+
+function logomouse() {
+  logo.addEventListener("mouseenter", function () {
+    crsr.style.scale = 6;
+  });
+  logo.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+  });
+}
+
+heromouse();
+logomouse();
+customcrsr();
