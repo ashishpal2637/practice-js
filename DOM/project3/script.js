99cc1e9aa5867931963f55717548e38e5bf49c2b
ashishpal2637 <ashishpal2637@gmail.com>
2025-08-07 10:26:04 +0530

    project3 completed

---
+++ DOM/project3/script.js
@@ -0,0 +1,10 @@
+var container = document.querySelector('.container');
+var love = document.querySelector('i');
+
+container.addEventListener('dblclick', function(){
+    love.style.transform = 'translate(-50%, -50%) scale(1)'
+
+    setTimeout(function() {
+        love.style.transform = 'translate(-50%, -50%) scale(0)'
+    }, 1000)
+})
